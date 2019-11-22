b6d68a9d022b0ef19916a86bb45cf1906e3bc225
Kevin Natera <mycityneedsme@Dat-Boiis-Toaster.local>
2019-11-22 12:19:50 -0500

    Initial Commit

---
+++ FireBaseProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FireBaseProject
+//
+//  Created by Kevin Natera on 11/22/19.
+//  Copyright © 2019 Kevin Natera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
