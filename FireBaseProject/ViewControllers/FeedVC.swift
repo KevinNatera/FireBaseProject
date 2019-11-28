ee5884e01b2cbc7650a8ef35c75cf6872f5df600
Kevin Natera <mycityneedsme@Dat-Boiis-Toaster.local>
2019-11-27 22:17:57 -0500

    Created ProfileVC

--- FireBaseProject/ViewControllers/FeedVC.swift
+++ FireBaseProject/ViewControllers/FeedVC.swift
@@ -8,22 +8,70 @@
 import UIKit
 
 class FeedVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var feedTableOutlet: UITableView!
+    
+    var user = AppUser(from: FirebaseAuthService.manager.currentUser!)
+    
+    var posts = [Post]() {
+        didSet {
+           
+            feedTableOutlet.reloadData()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let detailVC = segue.destination as? PostDetailVC else { fatalError() }
+        
+        let post = posts[feedTableOutlet.indexPathForSelectedRow!.row]
+        
+        detailVC.post = post
+        
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        getPosts()
+        
+      
+    }
+    
+    private func setup() {
+        feedTableOutlet.delegate = self
+        feedTableOutlet.dataSource = self
+        feedTableOutlet.rowHeight = 200
+    }
+    
+    
+    private func getPosts(){
+        
+        FirestoreService.manager.getAllPost { (result) in
+            DispatchQueue.main.async {
+                switch result{
+                case .failure(let error):
+                    print(error)
+                case .success(let data):
+                    self.posts = data.filter { (post) -> Bool in
+                        return post.creatorID != self.user.uid
+                    }
+                }
+                
+            }
+        }}
+    
+}
 
+extension FeedVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = feedTableOutlet.dequeueReusableCell(withIdentifier: "post") as! FeedTableViewCell
+        let post = posts[indexPath.row]
+        cell.configureCell(post: post)
+        return cell
+    }
+    
 }
--- FireBaseProject/ViewControllers/ProfileVC.swift
+++ FireBaseProject/ViewControllers/ProfileVC.swift
@@ -10,21 +10,121 @@ import UIKit
 
 class ProfileVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var user = AppUser(from: FirebaseAuthService.manager.currentUser!)
+    
+    
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var userNameTextFieldOutlet: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    @IBAction func photoButtonPressed(_ sender: UIButton) {
+        let imagePickerViewController = UIImagePickerController()
+        imagePickerViewController.delegate = self
+        imagePickerViewController.sourceType = .photoLibrary
+        present(imagePickerViewController, animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func confirmButtonPressed(_ sender: UIButton) {
+        guard let newUserName = userNameTextFieldOutlet.text else {
+            showAlert(title: "No username entered", message: "Please enter a new username.")
+            return
+        }
+        
+        guard let imageData = imageOutlet.image?.jpegData(compressionQuality: 1.0) else {
+            showAlert(title: "No image selected", message: "Please select a profile image.")
+            return
+        }
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        FirebaseStorage.profileManager.storeImage(image: imageData, completion: { [weak self] (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+                self!.disableSpinner()
+            case .success(let url):
+                FirebaseAuthService.manager.updateUserFields(userName: newUserName, photoURL: url) { (result) in
+                    switch result {
+                    case .failure(let error):
+                        print(error)
+                        self!.disableSpinner()
+                    case .success(()):
+                        FirestoreService.manager.updateCurrentUser(userName: newUserName, photoURL: url) { [weak self] (result) in
+                            switch result {
+                            case .failure(let error):
+                                print(error)
+                                self!.disableSpinner()
+                            case .success(()):
+                                self!.disableSpinner()
+                                self!.showAlert(title: "Success!", message: "Profile successfully updated.")
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        
+        
     }
-    */
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUp()
+    }
+    
+    private func setUp() {
+        activityIndicator.isHidden = true
+        if user.userName == nil {
+            userNameTextFieldOutlet.text = user.email
+        } else {
+            userNameTextFieldOutlet.text = user.userName
+        }
+        
+        if let photoUrl = user.photoURL {
+        FirebaseStorage.profileManager.getImages(profileUrl: photoUrl) { (result) in
+            switch result{
+            case .failure(let error):
+                self.imageOutlet.image = UIImage(named: "noImage")
+            case .success(let data):
+                self.imageOutlet.image = UIImage(data: data)
+            }
+        }
+        } else {
+            self.imageOutlet.image = UIImage(named: "noImage")
+        }
+    }
+    
+    private func disableSpinner() {
+        activityIndicator.isHidden = true
+        activityIndicator.stopAnimating()
+    }
+    
+    private func showAlert(title: String, message: String) {
+           let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+           alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+           present(alertVC, animated: true, completion: nil)
+       }
 
+}
+
+extension ProfileVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.originalImage] as? UIImage else {
+            return
+        }
+        
+        self.imageOutlet.image = image
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ProfileVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
 }
