f1c61cd38bf3cbaaf3ff1d8f9ce61c18d789bf36
Kevin Natera <mycityneedsme@Dat-Boiis-Toaster.local>
2019-11-27 22:13:31 -0500

    Created Post Model

--- FireBaseProject/Models/Post.swift
+++ FireBaseProject/Models/Post.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+struct Post {
+    let id: String
+    let creatorID: String
+    let dateCreated: Date?
+    let imageUrl: String?
+
+    
+    init( creatorID: String, dateCreated: Date? = nil , imageUrl: String? = nil){
+        self.creatorID = creatorID
+        self.dateCreated = dateCreated
+        self.imageUrl = imageUrl
+        self.id = UUID().description
+   
+    }
+    init? (from dict: [String: Any], id: String){
+        guard let userId = dict["creatorID"] as? String,
+         let userImage = dict["imageUrl"] as? String,
+    
+         let dateCreated = (dict["dateCreated"] as? Timestamp)?.dateValue() else { return nil }
+        self.creatorID = userId
+        self.id = id
+        self.dateCreated = dateCreated
+        self.imageUrl = userImage
+    }
+    var fieldsDict: [String: Any] {
+           return [
+               "creatorID": self.creatorID,
+               "imageUrl": self.imageUrl ?? "",
+           ]
+       }
+}
